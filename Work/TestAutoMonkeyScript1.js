da9d484d5d1d519d2229be57da8936a7565784e8
Admin <qq164093410@gmail.com>
2025-02-19 17:53:14 +0800

    ThinkPrompts Commit 1th by Pycharm 2025-03-24

---
+++ Work/TestAutoMonkeyScript1.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Auto Fill Form
+// @namespace   example
+// @version     1.0
+// @description 自动填写用户名和密码
+// @match       *://example.com/login*
+// @grant       none
+// ==/UserScript==
